e26fea786b00e519eb5ac347c49e77c2dca7fc37
Kausik Subramanian <sskausik08@gmail.com>
2015-11-04 13:34:22 -0600

    Added a Fat Tree Topology Generator

--- topologies/TopologyCreator.py
+++ topologies/TopologyCreator.py
@@ -5,6 +5,9 @@ class TopologyCreator(object):
 		if type == "clique" :
 			# Make a clique topology of k switches. 
 			self.clique(k)
+		elif type == "fattree" :
+			# Make a k-ary fat tree.
+			self.fattree(k)
 		else :
 			type == "random" 
 			# Make a random topology of k switches with atmost k/2 links for each switch.
@@ -29,10 +32,38 @@ class TopologyCreator(object):
 		# Make a random topology of k switches
 		pass
 
+	def fattree(self, k) :
+		# Make a k-ary Fat Tree. 
+		if not k%2 == 0 :
+			k = k - 1
+		edge = range(0,k*k/2)
+		aggregation = range(k*k/2, k*k)
+		core = range(k*k,k*k+(k*k/4))
+
+		f1 = open("./topoConf/switches", 'w')
+		f2 = open("./topoConf/links", 'w')
+
+		for e in edge :
+			f1.write("e" + str(e) + "\n")
+			pod = e / (k/2)
+			for off in range(0,k/2) :
+				agg = k * (k / 2) + (pod * (k/2)) + off
+				f2.write("e" + str(e) + " a" + str(agg) + "\n")				
+
+		for a in aggregation :
+			f1.write("a" + str(a) + "\n")
+			firstCore = k*k + (a % (k/2)) * (k/2)
+			for c in range(firstCore, firstCore + k/2) :
+				f2.write("a" + str(a) + " c" + str(c) + "\n")			
+
+		for c in core :
+			f1.write("c" + str(c) + "\n")
+
+
 
 
 #topotype = input("Enter type of topology ")
 #size = int(input('Enter number of switches '))
-ring = TopologyCreator("clique", 10)
+fattree = TopologyCreator("fattree", 4)
 
 		
