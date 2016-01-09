d774a55452fe71766a759ba7c6da1e3c230a898d
Kausik Subramanian <sskausik08@gmail.com>
2016-01-09 17:49:11 -0600

    Added Waypoint experiment files

---
+++ policyFiles/waypoint-exp2/experiment.sh
@@ -0,0 +1,5 @@
+touch waypoint2-timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-1.gpl >> waypoint2_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-2.gpl >> waypoint2_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-3.gpl >> waypoint2_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-4.gpl >> waypoint2_timing
