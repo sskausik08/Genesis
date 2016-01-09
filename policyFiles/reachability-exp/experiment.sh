09a444a5ae0566da22cd10b6a1c1c123a50ccca3
Kausik Subramanian <sskausik08@gmail.com>
2016-01-08 21:39:01 -0600

    Modified Reachability Experiment files

--- policyFiles/reachability-exp/experiment.sh
+++ policyFiles/reachability-exp/experiment.sh
@@ -1,11 +1,15 @@
 touch reachability-timing
-python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-100.gpl >> reachability-timing
-python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-200.gpl >> reachability-timing
-python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-300.gpl >> reachability-timing
-python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-400.gpl >> reachability-timing
-python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-500.gpl >> reachability-timing
-python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-600.gpl >> reachability-timing
-python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-700.gpl >> reachability-timing
-python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-800.gpl >> reachability-timing
-python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-900.gpl >> reachability-timing
-python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-1000.gpl >> reachability-timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-100.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-200.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-300.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-400.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-500.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-600.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-700.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-800.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-900.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-1000.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-1500.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-2000.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-2500.gpl >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/reachability-exp/fat6-3000.gpl >> reachability_timing
