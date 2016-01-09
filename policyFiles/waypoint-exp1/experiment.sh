011be85098155a6663a0ec83414bff22925e6648
Kausik Subramanian <sskausik08@gmail.com>
2016-01-08 22:07:56 -0600

    Added waypoint experiment 1 files

---
+++ policyFiles/waypoint-exp1/experiment.sh
@@ -0,0 +1,15 @@
+touch waypoint-timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-100.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-200.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-300.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-400.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-500.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-600.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-700.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-800.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-900.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-1000.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-1500.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-2000.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-2500.gpl >> waypoint_timing
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/waypoint-exp1/fat6-3000.gpl >> waypoint_timing
