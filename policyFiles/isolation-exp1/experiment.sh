f0774b2772f09dc88a2088286f44ebcc7dee6bf9
Kausik Subramanian <sskausik08@gmail.com>
2016-01-09 22:35:02 -0600

    Added Isolation Experiment 1

---
+++ policyFiles/isolation-exp1/experiment.sh
@@ -0,0 +1,5 @@
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/isolation-exp1/1.gpl -useTactic
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/isolation-exp1/2.gpl -useTactic
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/isolation-exp1/3.gpl -useTactic
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/isolation-exp1/4.gpl -useTactic
+python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/isolation-exp1/5.gpl -useTactic
