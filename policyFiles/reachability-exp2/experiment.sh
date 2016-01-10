3d56e5805503601ff5e1a7b1d6d7796e526b2746
Kausik Subramanian <sskausik08@gmail.com>
2016-01-10 00:21:15 -0600

    Added reachability exp for fat-8 tree

---
+++ policyFiles/reachability-exp2/experiment.sh
@@ -0,0 +1,15 @@
+touch reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-100.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-200.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-300.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-400.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-500.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-600.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-700.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-800.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-900.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-1000.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-1500.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-2000.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-2500.gpl -useTactic >> reachability8_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-3000.gpl -useTactic >> reachability8_timing
