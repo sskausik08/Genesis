691f0634278dff01d24703773c07290b2d995f8c
Kausik Subramanian <sskausik08@gmail.com>
2016-01-10 01:15:43 -0600

    Fixes in reachability experiment 2

--- policyFiles/reachability-exp2/experiment.sh
+++ policyFiles/reachability-exp2/experiment.sh
@@ -14,20 +14,20 @@ python GPLGenerator.py ./topologies/fattree-8.topo 2000 1 ./policyFiles/reachabi
 python GPLGenerator.py ./topologies/fattree-8.topo 2500 1 ./policyFiles/reachability-exp2/fat8-2500.gpl
 python GPLGenerator.py ./topologies/fattree-8.topo 3000 1 ./policyFiles/reachability-exp2/fat8-3000.gpl
 echo "8 topo" >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-100.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-200.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-300.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-400.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-500.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-600.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-700.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-800.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-900.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-1000.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-1500.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-2000.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-2500.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp/fat8-3000.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-100.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-200.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-300.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-400.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-500.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-600.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-700.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-800.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-900.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-1000.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-1500.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-2000.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-2500.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/reachability-exp2/fat8-3000.gpl -useTactic >> reachability_timing
 python GPLGenerator.py ./topologies/fattree-10.topo 100 1 ./policyFiles/reachability-exp2/fat8-100.gpl
 python GPLGenerator.py ./topologies/fattree-10.topo 200 1 ./policyFiles/reachability-exp2/fat8-200.gpl
 python GPLGenerator.py ./topologies/fattree-10.topo 300 1 ./policyFiles/reachability-exp2/fat8-300.gpl
@@ -43,20 +43,20 @@ python GPLGenerator.py ./topologies/fattree-10.topo 2000 1 ./policyFiles/reachab
 python GPLGenerator.py ./topologies/fattree-10.topo 2500 1 ./policyFiles/reachability-exp2/fat8-2500.gpl
 python GPLGenerator.py ./topologies/fattree-10.topo 3000 1 ./policyFiles/reachability-exp2/fat8-3000.gpl
 echo "10 topo" >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-100.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-200.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-300.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-400.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-500.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-600.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-700.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-800.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-900.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-1000.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-1500.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-2000.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-2500.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp/fat8-3000.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-100.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-200.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-300.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-400.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-500.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-600.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-700.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-800.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-900.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-1000.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-1500.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-2000.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-2500.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/reachability-exp2/fat8-3000.gpl -useTactic >> reachability_timing
 python GPLGenerator.py ./topologies/fattree-12.topo 100 1 ./policyFiles/reachability-exp2/fat8-100.gpl
 python GPLGenerator.py ./topologies/fattree-12.topo 200 1 ./policyFiles/reachability-exp2/fat8-200.gpl
 python GPLGenerator.py ./topologies/fattree-12.topo 300 1 ./policyFiles/reachability-exp2/fat8-300.gpl
@@ -72,17 +72,17 @@ python GPLGenerator.py ./topologies/fattree-12.topo 2000 1 ./policyFiles/reachab
 python GPLGenerator.py ./topologies/fattree-12.topo 2500 1 ./policyFiles/reachability-exp2/fat8-2500.gpl
 python GPLGenerator.py ./topologies/fattree-12.topo 3000 1 ./policyFiles/reachability-exp2/fat8-3000.gpl
 echo "12 topo" >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-100.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-200.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-300.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-400.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-500.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-600.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-700.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-800.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-900.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-1000.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-1500.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-2000.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-2500.gpl -useTactic >> reachability_timing
-python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp/fat8-3000.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-100.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-200.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-300.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-400.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-500.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-600.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-700.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-800.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-900.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-1000.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-1500.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-2000.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-2500.gpl -useTactic >> reachability_timing
+python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/reachability-exp2/fat8-3000.gpl -useTactic >> reachability_timing
