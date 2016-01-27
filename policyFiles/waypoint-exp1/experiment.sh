cac50145ec2629bedd6d5e910d6c62566a9bc159
Kausik Subramanian <sskausik08@gmail.com>
2016-01-27 10:58:17 -0600

    Minor fix in waypoint exp

--- policyFiles/waypoint-exp1/experiment.sh
+++ policyFiles/waypoint-exp1/experiment.sh
@@ -1,11 +1,11 @@
 touch waypoint1_timing
-python GPLGenerator.py ./topologies/fattree-6.topo 100 5 ./policyFiles/isolation-exp2/1.gpl
+python GPLGenerator6.py ./topologies/fattree-6.topo 100 5 ./policyFiles/isolation-exp2/1.gpl
 python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/isolation-exp2/1.gpl >> waypoint1_timing
-python GPLGenerator.py ./topologies/fattree-8.topo 100 5 ./policyFiles/isolation-exp2/1.gpl
+python GPLGenerator6.py ./topologies/fattree-8.topo 100 5 ./policyFiles/isolation-exp2/1.gpl
 python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/isolation-exp2/1.gpl >> waypoint1_timing
-python GPLGenerator.py ./topologies/fattree-10.topo 100 5 ./policyFiles/isolation-exp2/1.gpl
+python GPLGenerator6.py ./topologies/fattree-10.topo 100 5 ./policyFiles/isolation-exp2/1.gpl
 python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/isolation-exp2/1.gpl >> waypoint1_timing
-python GPLGenerator.py ./topologies/fattree-12.topo 100 5 ./policyFiles/isolation-exp2/1.gpl
+python GPLGenerator6.py ./topologies/fattree-12.topo 100 5 ./policyFiles/isolation-exp2/1.gpl
 python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/isolation-exp2/1.gpl >> waypoint1_timing
 
 
