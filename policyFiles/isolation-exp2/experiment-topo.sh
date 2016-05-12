8b036f6877d72a6b37c1f886e4bd6d34bf371d7c
Kausik Subramanian <sskausik08@gmail.com>
2016-05-12 15:32:31 -0500

    Changes to network repair experiment

--- policyFiles/isolation-exp2/experiment-topo.sh
+++ policyFiles/isolation-exp2/experiment-topo.sh
@@ -1,11 +1,11 @@
 touch isolation3_timing
-python GPLGenerator.py ./topologies/fattree-6.topo 25 5 ./policyFiles/isolation-exp2/1.gpl
+python GPLGenerator.py ./topologies/fattree-6.topo 15 5 ./policyFiles/isolation-exp2/1.gpl
 python -O genesis.py -topo ./topologies/fattree-6.topo -gpl ./policyFiles/isolation-exp2/1.gpl  >> isolation3_timing
-python GPLGenerator.py ./topologies/fattree-8.topo 65 5 ./policyFiles/isolation-exp2/1.gpl
+python GPLGenerator.py ./topologies/fattree-8.topo 30 5 ./policyFiles/isolation-exp2/1.gpl
 python -O genesis.py -topo ./topologies/fattree-8.topo -gpl ./policyFiles/isolation-exp2/1.gpl  >> isolation3_timing
-python GPLGenerator.py ./topologies/fattree-10.topo 125 5 ./policyFiles/isolation-exp2/1.gpl
+python GPLGenerator.py ./topologies/fattree-10.topo 60 5 ./policyFiles/isolation-exp2/1.gpl
 python -O genesis.py -topo ./topologies/fattree-10.topo -gpl ./policyFiles/isolation-exp2/1.gpl  >> isolation3_timing
-python GPLGenerator.py ./topologies/fattree-12.topo 215 5 ./policyFiles/isolation-exp2/1.gpl
+python GPLGenerator.py ./topologies/fattree-12.topo 100 5 ./policyFiles/isolation-exp2/1.gpl
 python -O genesis.py -topo ./topologies/fattree-12.topo -gpl ./policyFiles/isolation-exp2/1.gpl  >> isolation3_timing
 
 
