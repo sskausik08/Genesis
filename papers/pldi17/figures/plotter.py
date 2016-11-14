90c5070c065b5ffc5e649373abbf9c5b1ca7378c
Kausik Subramanian <sskausik08@gmail.com>
2016-11-13 23:19:12 -0600

    Updated plots

--- papers/pldi17/figures/plotter.py
+++ papers/pldi17/figures/plotter.py
@@ -55,13 +55,13 @@ plt.xlim(xmin=190, xmax=1010)
 
 plt.plot(x, ionOspfTime, '#ff7f00', marker="o", markersize=markersize, label="Best (Ion)")
 plt.errorbar(x, ionOspfTime, color='#ff7f00', yerr=ionOspfTimeDev, linestyle="None")
-plt.plot(x, ionOspfTimeWorst, '#377eb8',marker="^", markersize=markersize, label="Worst (Ion)")
-plt.errorbar(x, ionOspfTimeWorst, color='#377eb8', yerr=ionOspfTimeWorstDev, linestyle="None")
+plt.plot(x, ionOspfTimeWorst, '#ff7f00', linestyle='--', marker="^", markersize=markersize, label="Worst (Ion)")
+plt.errorbar(x, ionOspfTimeWorst, color='#ff7f00',  yerr=ionOspfTimeWorstDev, linestyle="None")
 
-plt.plot(x, fatOspfTime, '#4daf4a',marker="D", markersize=markersize, label="Best (Fat-8)")
-plt.errorbar(x, fatOspfTime, color='#4daf4a', yerr=fatOspfTimeDev, linestyle="None")
-plt.plot(x, fatOspfTimeWorst, '#984ea3', marker="s", markersize=markersize, label="Worst (Fat-8)")
-plt.errorbar(x, fatOspfTimeWorst, color='#984ea3', yerr=fatOspfTimeWorstDev, linestyle="None")
+plt.plot(x, fatOspfTime, '#377eb8',marker="D", markersize=markersize, label="Best (Fat-8)")
+plt.errorbar(x, fatOspfTime, color='#377eb8', yerr=fatOspfTimeDev, linestyle="None")
+plt.plot(x, fatOspfTimeWorst, '#377eb8', linestyle='--', marker="s", markersize=markersize, label="Worst (Fat-8)")
+plt.errorbar(x, fatOspfTimeWorst, color='#377eb8', yerr=fatOspfTimeWorstDev, linestyle="None")
 
 plt.legend(loc='upper left', frameon=False, fontsize=18)
 
@@ -91,13 +91,13 @@ plt.xlim(xmin=190, xmax=1010)
 
 plt.plot(x, ionConf, '#ff7f00', marker="o", markersize=markersize, label="Best (Ion)")
 plt.errorbar(x, ionConf, color='#ff7f00', yerr=ionConfDev, linestyle="None")
-plt.plot(x, ionConfWorst, '#377eb8',marker="^", markersize=markersize, label="Worst (Ion)")
-plt.errorbar(x, ionConfWorst, color='#377eb8', yerr=ionConfWorstDev, linestyle="None")
+plt.plot(x, ionConfWorst, '#ff7f00', linestyle='--', marker="^", markersize=markersize, label="Worst (Ion)")
+plt.errorbar(x, ionConfWorst, color='#ff7f00', yerr=ionConfWorstDev, linestyle="None")
 
-plt.plot(x, fatConf, '#4daf4a',marker="D", markersize=markersize, label="Best (Fat-8)")
-plt.errorbar(x, fatConf, color='#4daf4a', yerr=fatConfDev, linestyle="None")
-plt.plot(x, fatConfWorst, '#984ea3', marker="s", markersize=markersize, label="Worst (Fat-8)")
-plt.errorbar(x, fatConfWorst, color='#984ea3', yerr=fatConfWorstDev, linestyle="None")
+plt.plot(x, fatConf, '#377eb8',marker="D", markersize=markersize, label="Best (Fat-8)")
+plt.errorbar(x, fatConf, color='#377eb8', yerr=fatConfDev, linestyle="None")
+plt.plot(x, fatConfWorst, '#377eb8', linestyle='--', marker="s", markersize=markersize, label="Worst (Fat-8)")
+plt.errorbar(x, fatConfWorst, color='#377eb8', yerr=fatConfWorstDev, linestyle="None")
 
 plt.legend(loc='upper left', frameon=False, fontsize=18)
 
@@ -123,15 +123,15 @@ fatTRLWorstDev = [11.76737238,26.88723365,46.04162876,49.8160828,91.88750273]
 
 plt.xlim(xmin=190, xmax=1010)
 
-plt.plot(x, ionTRL, '#4daf4a',marker="D", markersize=markersize, label="Best (Ion)")
-plt.errorbar(x, ionTRL, color='#4daf4a', yerr=ionTRLDev, linestyle="None")
-plt.plot(x, ionTRLWorst, '#377eb8',marker="^", markersize=markersize, label="Worst (Ion)")
-plt.errorbar(x, ionTRLWorst, color='#377eb8', yerr=ionTRLWorstDev, linestyle="None")
+plt.plot(x, ionTRL, '#ff7f00',marker="o", markersize=markersize, label="Best (Ion)")
+plt.errorbar(x, ionTRL, color='#ff7f00', yerr=ionTRLDev, linestyle="None")
+plt.plot(x, ionTRLWorst, '#ff7f00', linestyle='--', marker="^", markersize=markersize, label="Worst (Ion)")
+plt.errorbar(x, ionTRLWorst, color='#ff7f00',   yerr=ionTRLWorstDev, linestyle="None")
 
-plt.plot(x, fatTRL, '#ff7f00', marker="o", markersize=markersize, label="Best (Fat-8)")
-plt.errorbar(x, fatTRL, color='#ff7f00', yerr=fatTRLDev, linestyle="None")
-plt.plot(x, fatTRLWorst, '#984ea3', marker="s", markersize=markersize, label="Worst (Fat-8)")
-plt.errorbar(x, fatTRLWorst, color='#984ea3', yerr=fatTRLWorstDev, linestyle="None")
+plt.plot(x, fatTRL, '#377eb8', marker="D", markersize=markersize, label="Best (Fat-8)")
+plt.errorbar(x, fatTRL, color='#377eb8', yerr=fatTRLDev, linestyle="None")
+plt.plot(x, fatTRLWorst, '#377eb8', linestyle='--', marker="s", markersize=markersize, label="Worst (Fat-8)")
+plt.errorbar(x, fatTRLWorst, color='#377eb8', yerr=fatTRLWorstDev, linestyle="None")
 
 plt.legend(loc='upper left', frameon=False, fontsize=18)
 
@@ -182,65 +182,113 @@ x = range(25, 225, 25)
 
 ionTime = [0.01749955864,0.07191940294,0.1476163773,0.2930797994,0.4789884697,0.6663588462,0.9209012076,1.15511427]
 ionTimeDev = [0.009488063792,0.02699395579,0.06919721199,0.08314940039,0.1465465516,0.2193709322,0.241433187,0.2587683784]
+ionTime = [p*t for p,t in zip(x,ionTime)]
+ionTimeDev = [p*t for p,t in zip(x,ionTimeDev)]
 
 fat6Time =  [0.04263912741,0.3389375772,2.070554649,4.668311028,7.679610742,10.82252409,12.59832289,15.4758605]
 fat6TimeDev = [0.02472738843,0.1789896801,1.013203957,0.9042154791,1.170098253,0.9448482264,1.129939945,1.686918172]
+fat6Time = [p*t for p,t in zip(x,fat6Time)]
+fat6TimeDev = [p*t for p,t in zip(x,fat6TimeDev)]
 
 
 fat4Time =  [0.02526011467,0.1084675329,0.2238150358,0.3463366435,0.4060181068,0.4730815102,0.5297162876,0.598101095]
 fat4TimeDev = [0.01213860087,0.0303217649,0.03861147015,0.0453386104,0.05625595682,0.04384863373,0.05146077266,0.07802651106]
+fat4Time = [p*t for p,t in zip(x,fat4Time)]
+fat4TimeDev = [p*t for p,t in zip(x,fat4TimeDev)]
 
 plt.xlim(xmin=15, xmax=210)
+plt.yscale('log')
 
 plt.plot(x, ionTime, '#4daf4a',marker="D", markersize=markersize, label="Geant (40)")
 plt.errorbar(x, ionTime, color='#4daf4a', yerr=ionTimeDev, linestyle="None")
 
+plt.plot(x, fat4Time, '#377eb8', marker="s", markersize=markersize, label="Fat-4 (20)")
+plt.errorbar(x, fat4Time, color='#377eb8', yerr=fat4TimeDev, linestyle="None")
+
 plt.plot(x, fat6Time, '#ff7f00', marker="o", markersize=markersize, label="Fat-6 (45)")
 plt.errorbar(x, fat6Time, color='#ff7f00', yerr=fat6TimeDev, linestyle="None")
 
-plt.plot(x, fat4Time, '#377eb8', marker="o", markersize=markersize, label="Fat-4 (20)")
-plt.errorbar(x, fat4Time, color='#377eb8', yerr=fat4TimeDev, linestyle="None")
 
 plt.legend(loc='upper left', frameon=False, fontsize=18)
 
 plt.xlabel('Number of Paths', fontsize=20)
-plt.ylabel('Average time per path', fontsize=20)
+plt.ylabel('OSPF Synthesis Time(log s)', fontsize=20)
 
 plt.grid()
 plt.savefig('ospfTime.eps', format='eps', dpi=1000, bbox_inches='tight')
 
-
-ospfTRLFig = plt.figure(6)
+ospfRFFig = plt.figure(6)
 x = range(25, 225, 25)
 
-ionTRL = [0.0176,0.062,0.07706666667,0.1091549296,0.13232,0.1640952381,0.1919163763,0.2178]
-ionTRLDev = [0.03040945073,0.04865137739,0.04052176706,0.06355529603,0.05495728954,0.06983110849,0.05786649815,0.05979693528]
+fat6RF = [6.966666667,41.23333333,173.0333333,434.3157895,742.0333333,1107.925926,1401.571429,1861.85]
+fat6RFDev = [4.902380374,13.54859284,63.37381344,71.21723623,78.4093957,83.4823454,94.13915176,134.8415347]
 
-fat6TRL =  [0.06,0.1486666667,0.2622222222,0.5684210526,0.8386666667,1.197530864,1.352040816,1.611]
-fat6TRLDev = [0.08187458871,0.09287490041,0.1431911439,0.1562854638,0.1853692184,0.1683722793,0.1681473585,0.1539617177]
+ionRF =  [4.78,25.28571429,50.24,99.45070423,157.48,224.5428571,320.4390244,406.92]
+ionRFDev = [3.677232766,8.881086983,18.92580573,29.9493805,38.19832243,60.74815813,63.74168525,68.83397358]
 
-fat4TRL =  [0.16,0.311,0.5293333333,0.695,0.7516,0.8403333333,0.8585714286,0.9165]
-fat4TRLDev = [0.131709409,0.1161623184,0.08771031533,0.1559520844,0.1284852644,0.09732023464,0.07427125365,0.1090400699]
+fat4RF =  [12.85,61.4,142.25,248.7,332.5,419.05,501.5,594.9]
+fat4RFDev = [6.722115895,15.08711545,22.1332686,32.69170183,38.80518142,39.33054926,44.07469736,68.99725395]
 
 plt.xlim(xmin=15, xmax=210)
-plt.ylim(ymin=0, ymax=2)
+plt.yscale('log')
 
-plt.plot(x, ionTRL, '#4daf4a',marker="D", markersize=markersize, label="Geant (40)")
-plt.errorbar(x, ionTRL, color='#4daf4a', yerr=ionTRLDev, linestyle="None")
+plt.plot(x, ionRF, '#4daf4a',marker="D", markersize=markersize, label="Geant (40)")
+plt.errorbar(x, ionRF, color='#4daf4a', yerr=ionRFDev, linestyle="None")
 
-plt.plot(x, fat6TRL, '#ff7f00', marker="o", markersize=markersize, label="Fat-6 (45)")
-plt.errorbar(x, fat6TRL, color='#ff7f00', yerr=fat6TRLDev, linestyle="None")
+plt.plot(x, fat4RF, '#377eb8', marker="o", markersize=markersize, label="Fat-4 (20)")
+plt.errorbar(x, fat4RF, color='#377eb8', yerr=fat4RFDev, linestyle="None")
 
-plt.plot(x, fat4TRL, '#377eb8', marker="o", markersize=markersize, label="Fat-4 (20)")
-plt.errorbar(x, fat4TRL, color='#377eb8', yerr=fat4TRLDev, linestyle="None")
+plt.plot(x, fat6RF, '#ff7f00', marker="o", markersize=markersize, label="Fat-6 (45)")
+plt.errorbar(x, fat6RF, color='#ff7f00', yerr=fat6RFDev, linestyle="None")
 
 plt.legend(loc='upper left', frameon=False, fontsize=18)
 
 plt.xlabel('Number of Paths', fontsize=20)
-plt.ylabel('Average resilience loss per path', fontsize=20)
+plt.ylabel('Total number of Route-Filters (log n)', fontsize=20)
 
 plt.grid()
-plt.savefig('ospfTRL.eps', format='eps', dpi=1000, bbox_inches='tight')
+plt.savefig('ospfRF.eps', format='eps', dpi=1000, bbox_inches='tight')
+
+ospfAvgResFig = plt.figure(7)
+x = range(25, 225, 25)
+
+ionAvgRes = [0.828,0.8251428571,0.7970666667,0.7521126761,0.74192,0.7087619048,0.6730313589,0.6447]
+ionAvgResDev = [0.2500285698,0.1814494209,0.1528632843,0.09906585822,0.08725936942,0.08120305442,0.0666334744,0.06690238091]
+ionBestRes = [21.14,44.35714286,65.56,86.12676056,109.28,130.9285714,151.3658537,172.5]
+
+fat6AvgRes =  [3.16,3.001333333,2.901777778,2.72,2.319733333,1.964691358,1.832653061,1.5545]
+fat6AvgResDev = [0.3528015951,0.3403419511,0.224519467,0.2138015695,0.1985601504,0.1381540675,0.0930914539,0.1425971506]
+fat6BestRes = [80.5,157.5,237.3,328.8421053,394.8,474.3333333,557.3214286,633.1]
+
+fat4AvgRes =  [1.476,1.261,1.086666667,0.938,0.8628,0.785,0.7557142857,0.717]
+fat4AvgResDev = [0.233201968,0.1280583748,0.1298852395,0.1101482255,0.1131154976,0.07114354158,0.07503096389,0.08324662155]
+fat4BestRes = [40.9,78.6,121.2,163.3,201.8,243.8,282.5,326.7]
+
+
+plt.xlim(xmin=15, xmax=210)
+plt.ylim(ymin=0, ymax=5)
+
+plt.plot(x, ionAvgRes, '#4daf4a',marker="D", markersize=markersize, label="Syn (40)")
+plt.plot(x, [res/x1 for x1,res in zip(x, ionBestRes)], '#4daf4a', linestyle='--', linewidth=4, marker="D", markersize=8, label="Best (40)")
+plt.errorbar(x, ionAvgRes, color='#4daf4a', yerr=ionAvgResDev, linestyle="None")
+
+plt.plot(x, fat4AvgRes, '#377eb8', marker="s", markersize=markersize, label="Syn (20)")
+plt.plot(x, [res/x1 for x1,res in zip(x, fat4BestRes)], '#377eb8', linestyle='--', linewidth=4, marker="s", markersize=8, label="Best (20)")
+plt.errorbar(x, fat4AvgRes, color='#377eb8', yerr=fat4AvgResDev, linestyle="None")
+
+
+plt.plot(x, fat6AvgRes, '#ff7f00', marker="o", markersize=markersize, label="Syn (45)")
+plt.plot(x, [res/x1 for x1,res in zip(x, fat6BestRes)], '#ff7f00', linestyle='--', linewidth=4, marker="o", markersize=8, label="Best (45)")
+plt.errorbar(x, fat6AvgRes, color='#ff7f00', yerr=fat6AvgResDev, linestyle="None")
+
+
+plt.legend(loc='upper right', mode="expand", ncol=3, frameon=False, fontsize=18)
+
+plt.xlabel('Number of Paths', fontsize=20)
+plt.ylabel('Average endpoint resilience', fontsize=20)
+
+plt.grid()
+plt.savefig('ospfAvgRes.eps',  format='eps', dpi=1000, bbox_inches='tight')
 # mcmc = plt.figure(2)
 # f = open("zep-mcmc.csv")
 # x = []
