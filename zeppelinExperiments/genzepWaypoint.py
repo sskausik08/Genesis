489b28d8b950b53c5232c23cbe7c32773bb4566a
Kausik Subramanian <sskausik08@gmail.com>
2017-04-07 19:37:16 -0500

    Waypoint compliance experiment amended

--- zeppelinExperiments/genzepWaypoint.py
+++ zeppelinExperiments/genzepWaypoint.py
@@ -6,7 +6,7 @@ import random
 
 if __name__ == "__main__":
 	waypointGroups = [2, 5, 10]
-	gpl_sizes = range(10,110,10)
+	gpl_sizes = range(10,90,10)
 	topofile = "./topologies/fattree-6.topo"
 	useTactic = False
 	tactic = "noEdgeLen7" # Tactics: noEdge, valleyFree, len7, noEdgeLen7
@@ -14,8 +14,8 @@ if __name__ == "__main__":
 
 	for pcs in gpl_sizes : 
 		for group in waypointGroups :
-			subprocess.call(["python", "GPLGenerator8.py", topofile, str(pcs), str(group), "1.gpl"])
 			for i in range(10) : 
+				subprocess.call(["python", "GPLGenerator8.py", topofile, str(pcs), str(group), "1.gpl"])
 				workloadArgs = ["python", "-O", "genesis.py", "-c3", "-ospf", "-topo", topofile, "-gpl", "1.gpl"]
 				if useTactic :
 					workloadArgs.append("-useTactic")
